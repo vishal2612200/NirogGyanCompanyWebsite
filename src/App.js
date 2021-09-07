9bb32d462e17195d61461a35093797fd05598249
XtremeGood <manglavishesh64@gmail.com>
2021-09-07 16:13:20 +0530

    react router dom switch used now

--- src/App.js
+++ src/App.js
@@ -12,42 +12,64 @@ import MailingList from './components/MailingList';
 import { useReducer, useState } from 'react';
 import specs from "./components/specs/home"
 import Footer from "./components/Footer"
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 export const PageContext = createContext()
+
+
+const Home = ({ state }) => {
+  return <React.Fragment>
+    <HeroSection state={state.HeroSection} />
+    <Brand state={state.Brand} />
+    <Features state={state.Features} />
+    <Services state={state.Services} />
+    <Bytes state={state.Bytes} />
+    <Report state={state.Report} />
+    <Achievements state={state.Achievements} />
+    <ClientMessage state={state.ClientMessage} />
+    <MailingList state={state.MailingList} />
+
+  </React.Fragment>
+}
+
+const About = ({ state }) => {
+  return <React.Fragment>
+    <Brand state={state.Brand} />
+    <Bytes state={state.Bytes} />
+    <Report state={state.Report} />
+    <Achievements state={state.Achievements} />
+    <ClientMessage state={state.ClientMessage} />
+    <MailingList state={state.MailingList} />
+  </React.Fragment>
+}
 
 function App() {
 
   const [state, setState] = useReducer(function (state, action) { }, specs);
   const [page, setPage] = useState("home");
-  let pageData;
-  switch (page) {
-
-    case "about":
-      pageData = (<React.Fragment>
-        <Brand state={state.Brand} />
-
-      </React.Fragment>)
-      break;
-    default:
-      pageData = (<React.Fragment >
-        <HeroSection state={state.HeroSection} />
-        <Brand state={state.Brand} />
-        <Features state={state.Features} />
-        <Services state={state.Services} />
-        <Bytes state={state.Bytes} />
-        <Report state={state.Report} />
-        <Achievements state={state.Achievements} />
-        <ClientMessage state={state.ClientMessage} />
-        <MailingList state={state.MailingList} />
-      </React.Fragment>)
-  }
 
   return (
     <div className="App">
       <PageContext.Provider value={{ page, setPage }}>
         <NavBar state={state.NavBar} />
       </PageContext.Provider>
-      {pageData}
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home state={state} />
+          </Route>
+          <Route path="/about">
+            <About state={state} />
+          </Route>
+          <Route path="/">
+            <Home state={state} />
+          </Route>
+        </Switch>
+      </Router>
       <Footer state={state.Footer} />
     </div>
   );
