2f25a965a01ea072ddb8484db368cd4eb42d76ea
XtremeGood <manglavishesh64@gmail.com>
2021-09-16 23:41:02 +0530

    heading width max removed md={10} now

--- src/components/utils/HeadingWithText.js
+++ src/components/utils/HeadingWithText.js
@@ -9,16 +9,13 @@ const useStyles = makeStyles({
     root: {
         padding: "3rem 0",
         textAlign: "center",
-        "&>div": {
-            width: "65%"
-        },
     }
 })
 
 export default function HeadingWithText({ content, ...props }) {
     const classes = useStyles();
     return <Grid container item className={classes.root} justifyContent="center" alignItems="center" {...props}>
-        <Grid item>
+        <Grid item md={10}>
 
             <Typography
                 variant="h3" gutterBottom
