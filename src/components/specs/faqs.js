209d2392eb9ba3ab5f39aa69e9be6196b20f60f3
Vishesh Mangla <manglavishesh64@gmail.com>
2021-09-29 10:48:02 +0530

    updated faqs data

--- src/components/specs/faqs.js
+++ src/components/specs/faqs.js
@@ -1,6 +1,7 @@
 import * as path from "path";
 
 
+
 import heroImage from "../faqs/static/imgs/hero-image.png"
 
 export default (function () {
@@ -27,33 +28,47 @@ export default (function () {
             cardsList: [
                 {
                     heading: "What does NirogGyan offer?",
-                    content:
-                        "For the 21st century patients, NirogGyan offers diagnostic test reports which are 'SmartReports' - all terms explained in simple language, interesting content, and world-class design. While a normal test report is just an A4 sheet with terms & measurements on it, our SmartReport is comprehensive but concise at the same time. Labs, doctors and patients in the post-pandemic world are all evolving with these informative reports.",
+                    content: "For the 21st century patients, NirogGyan offers diagnostic test reports which are 'SmartReports' - all terms explained in simple language, interesting content, and world-class design. While a normal test report is just an A4 sheet with terms & measurements on it, our SmartReport is comprehensive but concise at the same time. Labs, doctors and patients in the post-pandemic world are all evolving with these informative reports.",
                 },
                 {
-                    heading: "What does NirogGyan offer?",
-                    content:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel auctor mauris. Proin convallis lobortis risus,",
+                    heading: "What are the types of SmartReports available?",
+                    content: "NirogGyan covers 150+ tests in its Smart format, which include LFT, KFT, CBC, COVID-19 and almost every other test covered by pathology labs.\nWe offer SmartReports in 3 views:\n1) Advanced : Every test described in detail, with multiple actionable items\n2) Compact : Crisp information, smaller visualisation\n3) Dynamic : Focus on abnormal tests\n\n\nThe above three can be clubbed with these add-ons:\n1) Summary : Two-page report summary\n2) Hindi / Arabic : Entire SmartReport in the language of your choice\n3) Corporate Dashboard : Company-level health report",
                 },
                 {
-                    heading: "What does NirogGyan offer?",
-                    content:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel auctor mauris. Proin convallis lobortis risus,",
+                    heading: "What do doctors say about SmartReports?",
+                    content: "It is our endeavour to strengthen the relationship between healthcare professionals and patients, which is why we focus on simplification and making things easy-to-understand & easy-to-explain. Doctors who have encountered SmartReports through their patients have endorsed the concept, and report that their patients took the consultation more seriously.",
                 },
                 {
-                    heading: "What does NirogGyan offer?",
-                    content:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel auctor mauris. Proin convallis lobortis risus,",
+                    heading: "Are my personal details and diagnostic data secure with NirogGyan?",
+                    content: "Yes, your data is 100% safe with us.\n\nLike all major healthcare providers, we ensure that we don't consume any of your personal data (or contact details) in the first place and that we are legally bound never to disclose any identifiable test result information to third-parties.",
                 },
                 {
-                    heading: "What does NirogGyan offer?",
-                    content:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel auctor mauris. Proin convallis lobortis risus,",
+                    heading: "Where can I find NirogGyan’s SmartReports?",
+                    content: "If you own a pathology lab or a lab information system (LIS) company, you can delight your users with our service. Please reach out to us at +91-7678277891 or write an email to sales@niroggyan.com, and we’ll inform you about our subscription plans.\n  If you are a patient, drop us an email at contact@niroggyan.com and we’ll share our list of Smart labs with you, and facilitate your test booking as well.",
                 },
                 {
-                    heading: "What does NirogGyan offer?",
-                    content:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel auctor mauris. Proin convallis lobortis risus,",
+                    heading: "How much time will it take to integrate NirogGyan’s software with the pathology lab’s information systems?",
+                    content: "  The integration process takes less than 2 days. Do contact the customer care for more details.",
+                },
+                {
+                    heading: "How much time does the software take to process the test results and generate a SmartReport?",
+                    content: "  Less than 15 seconds!",
+                },
+                {
+                    heading: "What’s the pricing?",
+                    content: "We have subscription plans based on the offerings you choose and the volumes of SmartReports to be generated. Contact us to get the exact price quote",
+                },
+                {
+                    heading: "How can I rely on the content in SmartReports?",
+                    content: "NirogGyan’s policy regarding providing health-related content:‘be descriptive, not prescriptive’.  Our team of dedicated doctors and pharmacy graduates curate all the information through a ‘triple check’ process, before our software visualises and generates it on your SmartReport.",
+                },
+                {
+                    heading: "Who is behind NirogGyan?",
+                    content: "NirogGyan is founded by a group of IIM and BITS Pilani graduates, supported by medical professionals, techies, hustlers and startup-based organisations from all stages.",
+                },
+                {
+                    heading: "What if I want to cancel my subscription to NirogGyan’s software at a later stage?",
+                    content: "Don’t worry, you will absolutely love our offerings 🙂. That being said, if you do wish to discontinue, your subscription can be cancelled at any time by getting in touch with our customer care.\n",
                 },
             ]
         },
