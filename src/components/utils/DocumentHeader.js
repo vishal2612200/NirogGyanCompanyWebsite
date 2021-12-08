56531a3f9b831fb60035593d667fa767ee054d30
Yash <yash.nirrogyan@gmail.com>
2021-12-08 17:51:46 +0530

    back to original

--- src/components/utils/DocumentHeader.js
+++ src/components/utils/DocumentHeader.js
@@ -2,18 +2,11 @@ import { Helmet } from "react-helmet";
 
 export default function DocumentHeader({ state: documentHeader }) {
 
-    let dataRecieved = {
-        "title": documentHeader.title,
-        "description": documentHeader.description,
-        "canonical": documentHeader.canonical
-    }
-    console.log("dataRecieved\n", dataRecieved)
-
     return (
         <Helmet>
-            <title>{ dataRecieved.title }</title>
-            <meta name="description" content={dataRecieved.description} />
-            <link rel="canonical" href={dataRecieved.canonical} />
+            <title>{ documentHeader.title }</title>
+            <meta name="description" content={documentHeader.description} />
+            <link rel="canonical" href={documentHeader.canonical} />
         </Helmet>
     );
 }
