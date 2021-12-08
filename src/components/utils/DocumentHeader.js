11839ff85cce9465ae35bfc6e8eabac96ee12b4e
Yash <yash.nirrogyan@gmail.com>
2021-12-08 20:58:54 +0530

    new try7

--- src/components/utils/DocumentHeader.js
+++ src/components/utils/DocumentHeader.js
@@ -1,12 +1,27 @@
+import React, {useState, useEffect} from 'react'
 import { Helmet } from "react-helmet";
 
 export default function DocumentHeader({ state: documentHeader }) {
 
+    let collectData = {
+        "title": documentHeader.title,
+        "description": documentHeader.description,
+        "canonical": documentHeader.canonical
+    }
+
+    const [currentHeaders, setCurrentHeaders] = useState("")    
+
+    useEffect(() => {
+        setCurrentHeaders(collectData)
+    }, [])
+
+    console.log("currentHeaders\n", currentHeaders)
+
     return (
         <Helmet>
-            <title>{ documentHeader.title }</title>
-            <meta name="description" content={documentHeader.description} data-react-helmet="true" />
-            <link rel="canonical" href={documentHeader.canonical} data-react-helmet="true" />
+            <title>{ currentHeaders.title }</title>
+            <meta name="description" content={currentHeaders.description} data-react-helmet="true" />
+            <link rel="canonical" href={currentHeaders.canonical} data-react-helmet="true" />
         </Helmet>
     );
 }
