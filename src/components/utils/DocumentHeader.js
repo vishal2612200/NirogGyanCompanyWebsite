6ae7d751e7761da8ab7c90e9ef1ec5afe865af17
Yash <yash.nirrogyan@gmail.com>
2021-12-08 21:26:10 +0530

    new try9

--- src/components/utils/DocumentHeader.js
+++ src/components/utils/DocumentHeader.js
@@ -4,8 +4,9 @@ export default function DocumentHeader({ state: documentHeader }) {
 
     return (
         <Helmet>
-            <title>{documentHeader.title}</title>       
-            <meta name="description" content="App Description" />
+            <title>{ documentHeader.title }</title>
+            <meta name="description" content={documentHeader.description} />
+            <link rel="canonical" href={documentHeader.canonical} />
             <meta name="theme-color" content="#008f68" />
         </Helmet>
     );
