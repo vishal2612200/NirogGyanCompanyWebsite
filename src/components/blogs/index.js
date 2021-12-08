19f087c4290801a4cf414a4a51dfab05852c15ab
Yash <yash.nirrogyan@gmail.com>
2021-12-08 18:05:49 +0530

    url updates

--- src/components/blogs/index.js
+++ src/components/blogs/index.js
@@ -1,17 +1,24 @@
-import React, { useContext, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import HeroSection from './HeroSection';
 import Main from './Main';
 import { PageContext } from '../../App';
-import DocumentHeader from '../utils/DocumentHeader';
+//import DocumentHeader from '../utils/DocumentHeader';
+import { Helmet } from "react-helmet";
+
 
 export default function Blogs({ state, children }) {
-    const {  setPage } = useContext(PageContext);
+    const { setPage } = useContext(PageContext);
     useEffect(() => {
         setPage("blogs")
     })
 
     return <React.Fragment>
-        <DocumentHeader state={state.DocumentHeader} />
+        {/* <DocumentHeader state={state.DocumentHeader} /> */}
+        <Helmet>
+            <title>blogs page</title>
+            <meta name="description" content="this is your blogpage" />
+            <link rel="canonical" href="https://www.niroggyan.com/blogs" />
+        </Helmet>
         <HeroSection state={state.HeroSection} />
         <Main state={state.Main} />
         {children}
