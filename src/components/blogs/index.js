aa3bf56b8875bb1029a28f4f153000a1fc0ae1eb
Yash <yash.nirrogyan@gmail.com>
2021-12-08 17:36:21 +0530

    try4 url links

--- src/components/blogs/index.js
+++ src/components/blogs/index.js
@@ -1,17 +1,23 @@
-import React, { useContext, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import HeroSection from './HeroSection';
 import Main from './Main';
 import { PageContext } from '../../App';
 import DocumentHeader from '../utils/DocumentHeader';
+import { Helmet } from "react-helmet";
 
 export default function Blogs({ state, children }) {
-    const {  setPage } = useContext(PageContext);
+    const { setPage } = useContext(PageContext);
     useEffect(() => {
         setPage("blogs")
     })
 
     return <React.Fragment>
-        <DocumentHeader state={state.DocumentHeader} />
+        {/* <DocumentHeader state={state.DocumentHeader} /> */}
+        <Helmet>
+            <title>NirogGyan Executive Blogs & Updates on Medical Industry</title>
+            <meta name="description" content="NirogGyan features insights, trends, latest medical updates & opinions to bridge the gap in medical and health industry. Explore our blog section now." />
+            <link rel="canonical" href="https://www.niroggyan.com/blogs" />
+        </Helmet>
         <HeroSection state={state.HeroSection} />
         <Main state={state.Main} />
         {children}
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -7,13 +7,21 @@ import Features from './Features';
 import Report from './Report';
 import Achievements from './Achievements';
 import ClientMessage from './ClientMessage';
-import DocumentHeader from '../utils/DocumentHeader';
+//import DocumentHeader from '../utils/DocumentHeader';
+
+import { Helmet } from "react-helmet";
+
 
 export default function Home({ state }) {
 
     return (
         <React.Fragment>
-            <DocumentHeader state={state.DocumentHeader} />
+            {/* <DocumentHeader state={state.DocumentHeader} /> */}
+            <Helmet>
+                <title>NirogGyan - Smart & Modern Diagnostic Reports for Pathology Labs</title>
+                <meta name="description" content="Simplifying Medical Reports for Pathology Labs & Diagnosis centers. Hospitals can do Rebrand their image with Smart Reports can scale their revenue by 30%." />
+                <link rel="canonical" href="https://www.niroggyan.com/" />
+            </Helmet>
             <HeroSection state={state.HeroSection} />
             <Brand state={state.Brand} />
             <Features state={state.Features} />
