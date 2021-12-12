2a3d0e8c270a06e68ab0bbd37e39a0016bd453ca
Yash <yash.nirrogyan@gmail.com>
2021-12-12 22:58:22 +0530

    updated homepage

--- src/components/solutions/HeroSection.js
+++ src/components/solutions/HeroSection.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import HeroSecondary from "../utils/HeroSecondary"
+
 
 export default function HeroSection({ state: heroSection }) {
 
-    return <HeroSecondary state={heroSection} />
+    return (
+        <>
+            <Helmet>
+                <title>NirogGyan HomePage</title>
+                <meta name="description" content="home page description" />
+                <link rel="canonical" href="https://www.niroggyan.com/home" />
+                <meta name="theme-color" content="#008f68" />
+            </Helmet>
+
+            <HeroSecondary state={heroSection} />
+        </>
+    )
 
 }
