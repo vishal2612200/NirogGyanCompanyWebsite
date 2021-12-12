edfac09463dcd0cd432f503209cb8decdbb6b2e9
Yash <yash.nirrogyan@gmail.com>
2021-12-13 00:26:44 +0530

    just testing2

--- src/components/utils/DocumentHeader.js
+++ src/components/utils/DocumentHeader.js
@@ -4,10 +4,10 @@ export default function DocumentHeader({ state: documentHeader }) {
 
     return (
         <Helmet>
-            <title>{ documentHeader.title }</title>
+            {/* <title>{ documentHeader.title }</title>
             <meta name="description" content={documentHeader.description} />
             <link rel="canonical" href={documentHeader.canonical} />
-            <meta name="theme-color" content="#008f68" />
+            <meta name="theme-color" content="#008f68" /> */}
         </Helmet>
     );
 }
