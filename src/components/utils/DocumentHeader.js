3d0ab3ea5706bf535a5c587c0d53bcae2d2eb141
Yash <yash.nirrogyan@gmail.com>
2021-12-16 14:12:41 +0530

    react helmet try5

--- src/components/utils/DocumentHeader.js
+++ src/components/utils/DocumentHeader.js
@@ -3,20 +3,13 @@ import { Helmet } from "react-helmet";
 export default function DocumentHeader({ state: documentHeader }) {
 
     return (
-
-        <Helmet htmlAttributes={{ lang: 'en' }} title={documentHeader.title} meta={[
-            {
-                name: 'description',
-                content: documentHeader.description,
-            },
-        ]}
-            links={[
-                {
-                    rel: 'canonical',
-                    href: documentHeader.canonical,
-                },
-            ]}
-        />
-
+        <Helmet>
+            <meta
+                name="description"
+                content={documentHeader.description}
+                data-react-helmet="true"
+            />
+            <title>{documentHeader.title}</title>
+        </Helmet>
     );
 }
