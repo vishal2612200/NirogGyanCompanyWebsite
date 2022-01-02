4c5c35fb938ba285f0b4298fcf8e33a11d6466f3
Yash <yash.nirrogyan@gmail.com>
2022-01-02 17:26:41 +0530

    back to normal

--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { hydrate, render } from "react-dom";
+//import { hydrate, render } from "react-dom";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <>
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(
+//     <>
       
-      <App />
-    </>
-    , rootElement);
-} else {
-  render(
-    <>
-      <App />
-    </>
-    , rootElement);
-}
+//       <App />
+//     </>
+//     , rootElement);
+// } else {
+//   render(
+//     <>
+//       <App />
+//     </>
+//     , rootElement);
+// }
