7d24dd451c16d0d912f07038251aa3df3473a025
Yash <yash.nirrogyan@gmail.com>
2022-01-02 17:38:49 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import { hydrate, render } from "react-dom";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// const rootElement = document.getElementById("root");
-// if (rootElement.hasChildNodes()) {
-//   hydrate(
-//     <>
-      
-//       <App />
-//     </>
-//     , rootElement);
-// } else {
-//   render(
-//     <>
-//       <App />
-//     </>
-//     , rootElement);
-// }
+    , rootElement);
+} else {
+  render(
+    <App />
+    , rootElement);
+}
